550d047691d5a1d84e840f4e63885a5efcbde42b
nouhaila61 <nouhailaelbouhali99@gmail.com>
2023-04-23 22:46:59 +0100

    printf

--- main.h
+++ main.h
@@ -1,5 +1,5 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 #include <stdarg.h>
@@ -22,6 +22,5 @@ int _printf(const char *format, ...);
 int _putchar(char c);
 int print_c(va_list c);
 int print_s(va_list s);
-int (*check_format(const char *format))(va_list);
 
 #endif
