35d492cce3bc7715d5f4dbcb6de8cae4f01ff362
Mohammed el faqyh <simofinance22@gmail.com>
2023-04-24 16:49:27 -0700

    this is crazy shit

--- _printf.c
+++ _printf.c
@@ -8,49 +8,49 @@
  */
 int _printf(const char *format, ...)
 {
-    unsigned int i, len = 0;
-    va_list arg;
+	unsigned int i, len = 0;
+	va_list arg;
 
-    va_start(arg, format);
-    for (i = 0; format && format[i]; i++)
-    {
-        if (format[i] == '%')
-        {
-            i++;
-            while (format[i] == ' ')
-                i++;
+	va_start(arg, format);
+	for (i = 0; format && format[i]; i++)
+	{
+		if (format[i] == '%')
+	}
+	i++;
+	while (format[i] == ' ')
+		i++;
 
-            if (format[i] == '\0')
-                return (-1);
+	if (format[i] == '\0')
+		return (-1);
 
-            if (format[i] == '%')
-            {
-                len += _putchar('%');
-                continue;
-            }
-            else if (format[i] == 'c')
-                len += _putchar(va_arg(arg, int));
-            else if (format[i] == 's')
-            {
-                char *s = va_arg(arg, char *);
+	if (format[i] == '%')
+	{
+		len += _putchar('%');
+		continue;
+	}
+	else if (format[i] == 'c')
+		len += _putchar(va_arg(arg, int));
+	else if (format[i] == 's')
+	{
+		char *s = va_arg(arg, char *);
 
-                if (s == NULL)
-                    len += _puts("(null)");
-                else
-                    len += _puts(s);
-            }
-            else if (format[i] == 'd' || format[i] == 'i')
-                len += _print_integer(va_arg(arg, int));
-            else
-            {
-                len += _putchar('%');
-                len += _putchar(format[i]);
-            }
-        }
-        else
-            len += _putchar(format[i]);
-    }
-    va_end(arg);
+		if (s == NULL)
+			len += _puts("(null)");
+		else
+			len += _puts(s);
+	}
+	else if (format[i] == 'd' || format[i] == 'i')
+		len += _print_integer(va_arg(arg, int));
+	else
+	{
+		len += _putchar('%');
+		len += _putchar(format[i]);
+	}
+	{
+		else
+			len += _putchar(format[i]);
+	}
+	va_end(arg);
 
-    return (len);
+	return (len);
 }
