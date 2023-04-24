9c7d03f42f1f322c046f6d27d5823f57095e9c4e
nouhaila61 <nouhailaelbouhali99@gmail.com>
2023-04-24 05:22:18 +0100

    printf

--- functions.c
+++ functions.c
@@ -1,43 +1,5 @@
 #include "main.h"
 
-/**
- * print_number - print a number
- * @n:the number .
- * Return: the value o i
- *
- */
-
-int print_number(int n)
-{
-	int i = 0;
-
-	if (n == -2147483648)
-	{
-		_putchar('-');
-		i++;
-		_putchar('2');
-		i++;
-		i += print_number(147483648);
-	}
-	else if (n < 0)
-	{
-		_putchar('-');
-		i++;
-		n *= -1;
-		i += print_number(n);
-	}
-	else if (n > 9)
-	{
-		i += print_number(n / 10);
-		i += print_number(n % 10);
-	}
-	else if (n <= 9)
-	{
-		_putchar(n + 48);
-		i++;
-	}
-	return (i);
-	}
 
 /**
  * _putchar - writes a character to stdout
