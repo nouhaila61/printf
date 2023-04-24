347514b814a38b3c8bb32891f0786fefa6d48c03
nouhaila61 <nouhailaelbouhali99@gmail.com>
2023-04-24 03:44:12 +0100

    printf

--- _printf.c
+++ _printf.c
@@ -6,36 +6,31 @@
  * @i: current position in the format string
  * @arg: variable argument list
  * @len: current length of the output
+ * @tmpi: holds th value of i
  * Return: the updated length of the output
  */
-int t_print(const char *str, int i, va_list arg, int len)
+
+int	t_print(const char *str, int i, va_list arg, int len, int tmpi)
 {
-	char *s;
-	int n;
-
-	switch (str[i + 1])
+	if (str[i + 1] == 'c')
+		len += _putchar(va_arg(arg, int));
+	else if (str[i + 1] == 's')
+		len += _putstring(va_arg(arg, char *));
+	else if (str[i + 1] == 'd' || str[i + 1] == 'i')
+		len += print_number(va_arg(arg, int));
+	else if (str[i + 1] == '%')
+		len += _putchar('%');
+	else if (str[i + 1] == 'b')
+		len += convert_binary(va_arg(arg, int));
+	else if (str[i + 1])
 	{
-		case 'c':
-			len += _putchar(va_arg(arg, int));
-			break;
-		case 's':
-			s = va_arg(arg, char *);
-			if (s == NULL)
-				s = "(null)";
-			len += _putstring(s);
-			break;
-		case '%':
-			len += _putchar('%');
-			break;
-		default:
-			len += _putchar(str[i]);
-			len += _putchar(str[i + 1]);
-			break;
+		len += _putchar(str[tmpi]);
+		_putchar(str[tmpi + 1]);
+		len++;
+		i++;
 	}
-
 	return (len);
 }
-
 /**
  * _printf - produces output according to a format
  * @format: a string containing zero or more directives
@@ -44,28 +39,42 @@ int t_print(const char *str, int i, va_list arg, int len)
  */
 int _printf(const char *format, ...)
 {
-	int i = 0, len = 0;
+	unsigned int i;
+	unsigned int len;
 	va_list arg;
-
-	if (format == NULL)
-		return (-1);
+	int tmpi;
 
 	va_start(arg, format);
-
-	while (format[i])
+	i = 0;
+	tmpi = 0;
+	len = 0;
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	if (format != NULL)
 	{
-		if (format[i] == '%')
+		while (format[i])
 		{
-			len = t_print(format, i, arg, len);
+			if (format[i] == '%')
+			{
+				tmpi = i;
+				while (format[i + 1] == ' ')
+				{
+					i++;
+					if (format[i + 1] != ' ')
+					{
+						break;
+					}
+				}
+				len = t_print(format, i, arg, len, tmpi);
+				i++;
+			}
+			else if (format[i])
+				len += _putchar(format[i]);
 			i++;
 		}
-		else
-			len += _putchar(format[i]);
-
-		i++;
 	}
-
 	va_end(arg);
-
 	return (len);
 }
--- main.h
+++ main.h
@@ -2,14 +2,17 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <stdarg.h>
 #include <unistd.h>
+#include <stdarg.h>
 #include <stdlib.h>
+#include <limits.h>
 
-
-int _printf(const char *format, ...);
+int print_number(int n);
 int _putchar(char c);
 int _putstring(char *s);
-int t_print(const char *str, int i, va_list arg, int len);
+int	t_print(const char *str, int i, va_list arg, int len, int tmpi);
+int _printf(const char *format, ...);
+int convert_binary(int num);
+void print_rev(int *store, int size);
 
 #endif
