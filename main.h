c10fb8a6944e2beedf65dfeceab5ea9e8c860330
nouhaila61 <nouhailaelbouhali99@gmail.com>
2023-04-24 05:02:29 +0100

    printf

--- main.h
+++ main.h
@@ -12,5 +12,7 @@ int _putchar(char c);
 int _putstring(char *s);
 int t_print(const char *str, int i, va_list arg, int len, int tmpi);
 int _printf(const char *format, ...);
+int convert_binary(int num);
+void print_rev(int *store, int size);
 
 #endif
